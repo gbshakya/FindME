670f08baa9f2f01a4051f3e39550431f87a8b381
Salina Koirala <46720934+SalinaK1@users.noreply.github.com>
2020-09-23 02:20:26 +0545

    Multiple user created and tested locally

--- accounts/models.py
+++ accounts/models.py
@@ -6,5 +6,4 @@ class User(AbstractUser):
     is_employer = models.BooleanField('employer_status', default=False)
     is_employee = models.BooleanField('employee_status', default=False)
     is_admin = models.BooleanField('admin_status', default=False)
-    test_field = models.BooleanField('test', default=False)
     
