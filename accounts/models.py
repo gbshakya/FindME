0241a48e2e9bd667ed4fa63205d973097d5ed1d1
Salina Koirala <46720934+SalinaK1@users.noreply.github.com>
2020-09-23 11:21:53 +0545

    Custom user model created and tested

--- accounts/models.py
+++ accounts/models.py
@@ -3,5 +3,8 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+    is_employer = models.BooleanField('employer_status', default=False)
+    is_employee = models.BooleanField('employee_status', default=False)
+    is_admin = models.BooleanField('admin_status', default=False)
+    
     
